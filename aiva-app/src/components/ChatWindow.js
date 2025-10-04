e9f09b8e87c2834384597d37ae339d6e9b93df96
Veera Kettunen <veera.kettunen@student.laurea.fi>
2025-10-04 09:46:07 +0300

    Reactin pohjan luominen + frontendia
    Luotu uusi React-projekti (npx create-react-app my-app)
    
    Luot Chatbotin keskustelualue ja keskustelukuplat botille ja käyttäjälle

---
+++ aiva-app/src/components/ChatWindow.js
@@ -0,0 +1,22 @@
+//Komponentti chatbotin ikkunalle, jossa keskustelu käydään
+//Botille oma ikoni ja puhekupla (chat-message bot) ja käyttäjälle puhekupla (chat-message user)
+
+import aivaIcon from "../images/aiva.png";  
+import "./ChatWindow.css"
+
+function ChatWindow() {
+    return (
+        <div className="chat-window">
+            <div className="chat-message bot">
+                <img src={aivaIcon} alt="aiva-bot" className="chat-icon" />
+                <div className="chat-bubble bot-bubble">Hei! Miten voin auttaa sinua tänään?</div>
+            </div>
+            <div className="chat-message user">
+                <div className="chat-bubble user-bubble">Etsin uutta telkkaria</div>
+                <img src={aivaIcon} alt="aiva-bot" className="chat-icon" />
+            </div>
+        </div>
+    );
+}
+
+export default ChatWindow;
---
+++ aiva-app/src/components/Navbar.js
@@ -0,0 +1,19 @@
+//Header ja navigointiosa
+
+function Navbar() {
+    return (
+        <>
+            <header className="navbar">
+                <h1>Tervetuloa tv-ostoksille</h1>
+                <nav>
+                <ul>
+                <li>Kotisivu</li>
+                <li>About Us</li>
+                </ul>
+            </nav>
+            </header>
+        </>
+    );
+}
+
+export default Navbar;
