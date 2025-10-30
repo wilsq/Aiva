d543fa0ca4202bcc03ae33dbe70ddaf3da2403f9
Veera Kettunen <veera.kettunen@student.laurea.fi>
2025-10-30 20:25:10 +0200

    ProductView-komponentti
    Tuotelistauksesta voi klikata tuotetta ja ikkunaan avautuu ProductView ja takaisinnappulasta pääsee takaisin tuotelistaukseen

--- aiva-chatbot/frontend/src/components/ProductList.jsx
+++ aiva-chatbot/frontend/src/components/ProductList.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ProductView from "./ProductView";
 
 function ProductList({products}) {
+    const [valittuTuote, setValittuTuote] = useState(null);
+
     if (!Array.isArray(products) || products.length === 0) {
         return <p>Tuotteita ei löytynyt.</p>;
+    }
+
+    if (valittuTuote) {
+        return (
+            <ProductView product={valittuTuote} onBack={() => setValittuTuote(null)} />
+        );
     }
 
     return (
           <div className="tuotelista">
             <div className="tuotekaruselli">
                 {products.map((item, index) => (
-                    <div key={index} className="tulos">
+                    <div key={index} className="tulos" onClick={() => setValittuTuote(item)}>
                         <strong>{item.name}</strong> <br />
-                        <span>Hinta: {item.price}</span>
+                        <span>Hinta: {item.price}€</span>
                     </div>
             ))}
             </div>
--- aiva-chatbot/frontend/src/components/ProductView.jsx
+++ aiva-chatbot/frontend/src/components/ProductView.jsx
@@ -1,8 +1,22 @@
 //Tuotenäkymä, jos klikkaa hakulistauksessa olevaa tuotekorttia
 import React from "react";
 
-function ProductView() {
+function ProductView( { product, onBack } ) {
+    if (!product) return <p>Tuotetta ei löytynyt</p>;
 
+    return (
+        <div className="tuotenakyma">
+            <button onClick={onBack} className="takaisinNappula">Takaisin</button>
+
+            <h2>{product.name}</h2>
+            <p><strong>Hinta:</strong> {product.price}€</p>
+            <p><strong>Merkki:</strong> {product.brand}</p>
+            <p><strong>Malli:</strong> {product.model}</p>
+            <p><strong>Koko:</strong> {product.sizeInches}</p>
+            <p><strong>Kuvaus:</strong> {product.description}</p>
+        
+        </div>
+    );
 }
 
 export default ProductView;
