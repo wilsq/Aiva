d6ef1ebdb0df2f3e17106794c92167650d309ffc
wilsq <viljami.j.viinikainen@gmail.com>
2025-11-02 22:24:11 +0200

    Added imageUrl to productview

--- aiva-chatbot/frontend/src/components/ProductView.jsx
+++ aiva-chatbot/frontend/src/components/ProductView.jsx
@@ -1,22 +1,34 @@
 //Tuotenäkymä, jos klikkaa hakulistauksessa olevaa tuotekorttia
 import React from "react";
 
-function ProductView( { product, onBack } ) {
-    if (!product) return <p>Tuotetta ei löytynyt</p>;
+function ProductView({ product, onBack }) {
+  if (!product) return <p>Tuotetta ei löytynyt</p>;
 
-    return (
-        <div className="tuotenakyma">
-            <button onClick={onBack} className="takaisinNappula">Takaisin</button>
+  return (
+    <div className="tuotenakyma">
+      <button onClick={onBack} className="takaisinNappula">
+        Takaisin
+      </button>
+      <img src={product.imageUrl} alt={product.name} />
 
-            <h2>{product.name}</h2>
-            <p><strong>Hinta:</strong> {product.price}€</p>
-            <p><strong>Merkki:</strong> {product.brand}</p>
-            <p><strong>Malli:</strong> {product.model}</p>
-            <p><strong>Koko:</strong> {product.sizeInches}</p>
-            <p><strong>Kuvaus:</strong> {product.description}</p>
-        
-        </div>
-    );
+      <h2>{product.name}</h2>
+      <p>
+        <strong>Hinta:</strong> {product.price}€
+      </p>
+      <p>
+        <strong>Merkki:</strong> {product.brand}
+      </p>
+      <p>
+        <strong>Malli:</strong> {product.model}
+      </p>
+      <p>
+        <strong>Koko:</strong> {product.sizeInches}"
+      </p>
+      <p>
+        <strong>Kuvaus:</strong> {product.description}
+      </p>
+    </div>
+  );
 }
 
 export default ProductView;
